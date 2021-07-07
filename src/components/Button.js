db5bf327a21720c6ed00f9b3dd62b3ccfe38040d
CimaraOliveira <cimarinhaoliveira@hotmail.com>
2021-07-07 20:44:47 -0300

    criando calculadora

---
+++ src/components/Button.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import{StyleSheet,Text,Dimensions, TouchableHighlight} from 'react-native'
+
+const styles = StyleSheet.create({
+    button  : {
+        fontSize: 40,
+        height: Dimensions.get('window').width  /  5,
+        width: Dimensions.get('window').width / 5,
+        padding: 10,
+        textAlign: 'center',
+        backgroundColor: '#888',
+        borderWidth: 2,
+        borderColor: 'black',
+        color:'#fff',    
+        left: 1,
+        borderRadius:10,
+     
+    },
+
+    operationButton:{
+        color:'#fff',
+        backgroundColor:'#888',
+        borderRadius:10,
+       
+    },
+
+    buttonsesquerda:{
+        fontSize: 40,
+        height: 70,
+        width: Dimensions.get('window').width / 5,   
+        borderRadius:10, 
+      
+      
+        
+
+    },
+  
+    buttonDouble:{
+      height: (Dimensions.get('window').width  /  5) *2,
+      borderRadius:10,
+    },
+
+    buttonTriple:{
+        width:400,
+        left:30, 
+        height:80, 
+        marginTop:10,
+        fontSize: 40,
+        color:'#fff', 
+        borderRadius:10,
+        backgroundColor:'#FF8C00',
+        padding: 10,
+       
+         
+    },
+
+   
+})
+
+export default props => {
+    const stylesButton = [styles.button]
+    if (props.double) stylesButton.push(styles.buttonDouble)
+    if (props.triple) stylesButton.push(styles.buttonTriple)
+    if (props.operation) stylesButton.push(styles.operationButton)
+    if(props. buttonsesquerda) stylesButton.push(styles.buttonsesquerda)
+    return (
+        <TouchableHighlight onPress={() => props.onClick(props.label)}>
+            <Text style={stylesButton}> {props.label} </Text>
+        </TouchableHighlight>
+    )
+}
+
